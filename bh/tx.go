877d69554ad20dfb9774b2bf174a02fafa5c58a7
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2014-09-20 11:33:44 -0400

    Add IsEmpty and comments to transactions
    We add a method to check whether a transaction is empty. Also we add
    comments for all public methods and fields of Tx.

--- bh/tx.go
+++ bh/tx.go
@@ -2,11 +2,17 @@ package bh
 
 import "fmt"
 
-type TxSeq uint64
+// TxGeneration is the generation of the colony that began the transaction.
 type TxGeneration uint64
 
+// TxSeq represents a monotonically increasing number (with the possbility of a
+// wrap around) for the transaction.
+type TxSeq uint64
+
+// TxStatus represents the status of a transaction.
 type TxStatus uint8
 
+// Valid values for TxStatus
 const (
 	TxCommitted TxStatus = iota
 	TxOpen               = iota
@@ -15,29 +21,39 @@ const (
 // Tx represents the side effects of an operation: messages emitted during the
 // transaction as well as state operations.
 type Tx struct {
-	Seq        TxSeq
-	Generation TxGeneration
-	Msgs       []Msg
-	Ops        []StateOp
-	Status     TxStatus
+	Generation TxGeneration // Generation of the colony that began this tx.
+	Seq        TxSeq        // An motonically increasing sequence of the tx.
+	Msgs       []Msg        // Messages buffered in this tx.
+	Ops        []StateOp    // State operations in this tx.
+	Status     TxStatus     // Status of this tx.
 }
 
+// AddMsg adds a message to this transaction.
 func (t *Tx) AddMsg(msg Msg) {
 	t.Msgs = append(t.Msgs, msg)
 }
 
+// AddOp adds a state operation to this transaction.
 func (t *Tx) AddOp(op StateOp) {
 	t.Ops = append(t.Ops, op)
 }
 
+// IsOpen returns true if the transaction is open.
 func (t *Tx) IsOpen() bool {
 	return t.Status == TxOpen
 }
 
+// Reset simply clears messages and operations of the transaction. It does not
+// change the sequence nor the generation.
 func (t *Tx) Reset() {
 	t.Msgs = nil
 	t.Ops = nil
 	t.Status = TxCommitted
+}
+
+// IsEmpty returns true if there is no messages or operations in the tx.
+func (t *Tx) IsEmpty() bool {
+	return len(t.Msgs) == 0 && len(t.Ops) == 0
 }
 
 func (t Tx) String() string {
