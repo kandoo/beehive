98e5eabf2269ef47a2970c62032a6b8ada81b0fb
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2014-10-26 11:24:58 -0400

    Add test for hive failure

--- hive_test.go
+++ hive_test.go
@@ -6,6 +6,7 @@ import (
 	"runtime"
 	"strconv"
 	"testing"
+	"time"
 )
 
 const (
@@ -104,7 +105,7 @@ func TestHiveRestart(t *testing.T) {
 	runHiveTest(DefaultCfg, t)
 }
 
-func TestCluster(t *testing.T) {
+func TestHiveCluster(t *testing.T) {
 	cfg1 := DefaultCfg
 	cfg1.StatePath = "/tmp/bhtest1"
 	cfg1.Addr = "127.0.0.1:7767"
@@ -135,4 +136,51 @@ func TestCluster(t *testing.T) {
 	h3.Stop()
 	h2.Stop()
 	h1.Stop()
+}
+
+func TestHiveFailure(t *testing.T) {
+	cfg1 := DefaultCfg
+	cfg1.StatePath = "/tmp/bhtest1"
+	cfg1.Addr = "127.0.0.1:7767"
+	defer removeState(cfg1)
+	h1 := NewHiveWithConfig(cfg1)
+	go h1.Start()
+	waitTilStareted(h1)
+
+	cfg2 := DefaultCfg
+	cfg2.StatePath = "/tmp/bhtest2"
+	cfg2.Addr = "127.0.0.1:7777"
+	cfg2.PeerAddrs = []string{"127.0.0.1:7767"}
+	defer removeState(cfg2)
+	h2 := NewHiveWithConfig(cfg2)
+	go h2.Start()
+
+	cfg3 := DefaultCfg
+	cfg3.StatePath = "/tmp/bhtest3"
+	cfg3.Addr = "127.0.0.1:7787"
+	cfg3.PeerAddrs = []string{"127.0.0.1:7767"}
+	defer removeState(cfg3)
+	h3 := NewHiveWithConfig(cfg3)
+	go h3.Start()
+
+	waitTilStareted(h2)
+	waitTilStareted(h3)
+
+	h1.Stop()
+
+	for {
+		if _, err := h2.(*hive).processCmd(cmdSync{}); err == nil {
+			break
+		}
+		time.Sleep(10 * defaultRaftTick)
+	}
+	for {
+		if _, err := h3.(*hive).processCmd(cmdSync{}); err == nil {
+			break
+		}
+		time.Sleep(10 * defaultRaftTick)
+	}
+
+	h3.Stop()
+	h2.Stop()
 }
