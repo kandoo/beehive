311fd8c9fb47c3ab1e364e4f8cb8afee87c27e41
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2014-12-17 23:19:27 -0500

    Add Errorf to gob errors
    Change-Id: I527a2300f77834f32d4749dcca96fbcff464d612

--- gob/err.go
+++ gob/err.go
@@ -2,12 +2,17 @@ package gob
 
 import (
 	gogob "encoding/gob"
+	"fmt"
 )
 
 type Error string
 
 func (e Error) Error() string {
 	return string(e)
+}
+
+func Errorf(format string, args ...interface{}) Error {
+	return Error(fmt.Sprintf(format, args))
 }
 
 func init() {
