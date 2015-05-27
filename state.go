1903fe7533686dd7585f838f60c89c2f3b1bb8ca
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2015-05-26 21:07:51 -0400

    Remove the unused state from beehive
    Change-Id: Iaf735f0db649a43d3e5b0b0f3dba8f563d93d45a

--- state.go
+++
@@ -1,9 +0,0 @@
-package beehive
-
-import "github.com/kandoo/beehive/state"
-
-// State is the storage for a collection of dictionaries.
-type State interface {
-	// Returns a dictionary for this state. Creates one if it does not exist.
-	Dict(name string) state.Dict
-}
