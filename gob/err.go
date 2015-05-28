45b1b1bc66a1a2ef87cf4684272e448f21b42163
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2015-05-27 22:24:33 -0400

    Enchance gob.Error
    This commit adds a constructor for gob.Error as well as an IsNil method
    to check whether the error is nil.
    
    Change-Id: I49b33139df671a2f266a9b6ce84bbfadbb063945

--- gob/err.go
+++ gob/err.go
@@ -11,6 +11,17 @@ func (e Error) Error() string {
 	return string(e)
 }
 
+func (e Error) IsNil() bool {
+	return e == ""
+}
+
+func NewError(err error) Error {
+	if err == nil {
+		return ""
+	}
+	return Error(err.Error())
+}
+
 func Errorf(format string, args ...interface{}) Error {
 	return Error(fmt.Sprintf(format, args))
 }
